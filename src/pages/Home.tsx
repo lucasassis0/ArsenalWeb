64cdc1d280b3a86c359e517f6405ca47f6bba21e
Lucas Assis <lucas.assis_@hotmail.com>
2022-02-07 17:58:16 -0400

    implementa comunidade em home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -24,7 +24,7 @@ export default function Home() {
                                 className="d-block"
                                 src={require("../assets/img/thumb1.jpg")}
                                 alt="First slide"
-                                // width={600}
+                            // width={600}
                             />
                             <Carousel.Caption>
                                 <h3>First slide label</h3>
@@ -36,7 +36,7 @@ export default function Home() {
                                 className="d-block"
                                 src={require("../assets/img/thumb2.jpg")}
                                 alt="Second slide"
-                                // width={600}
+                            // width={600}
                             />
                             <Carousel.Caption>
                                 <h3>Second slide label</h3>
@@ -48,7 +48,7 @@ export default function Home() {
                                 className="d-block"
                                 src={require("../assets/img/thumb1.jpg")}
                                 alt="Third slide"
-                                // width={600}
+                            // width={600}
                             />
                             <Carousel.Caption>
                                 <h3>Third slide label</h3>
@@ -136,6 +136,175 @@ export default function Home() {
                         </div>
                     </div>
                 </div>
+            </div>
+            <div className={style.featuredContainer}>
+                <div className={style.featuredHeader}>
+                    <span>Destaques</span>
+                </div>
+                <div className={style.featured}>
+                    <div className={style.featuredCard}>
+                        <div>
+                            <img src={require("../assets/img/thumb1.jpg")} alt="" width="100%" />
+                        </div>
+
+                        <div className={style.featuredCardTitle}>
+                            <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, voluptates.</span>
+                        </div>
+                    </div>
+                    <div className={style.featuredCard}>
+                        <div>
+                            <img src={require("../assets/img/thumb2.jpg")} alt="" width="100%" />
+                        </div>
+                        <div className={style.featuredCardTitle}>
+                            <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, voluptates.</span>
+                        </div>
+                    </div>
+                    <div className={style.featuredCard}>
+                        <div>
+                            <img src={require("../assets/img/thumb3.jpg")} alt="" width="100%" />
+                        </div>
+                        <div className={style.featuredCardTitle}>
+                            <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, voluptates.</span>
+                        </div>
+                    </div><div className={style.featuredCard}>
+                        <div>
+                            <img src={require("../assets/img/thumb1.jpg")} alt="" width="100%" />
+                        </div>
+                        <div className={style.featuredCardTitle}>
+                            <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, voluptates.</span>
+                        </div>
+                    </div>
+                    <div className={style.featuredCard}>
+                        <div>
+                            <img src={require("../assets/img/thumb2.jpg")} alt="" width="100%" />
+                        </div>
+                        <div className={style.featuredCardTitle}>
+                            <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, voluptates.</span>
+                        </div>
+                    </div>
+                    <div className={style.featuredCard}>
+                        <div>
+                            <img src={require("../assets/img/thumb3.jpg")} alt="" width="100%" />
+                        </div>
+                        <div className={style.featuredCardTitle}>
+                            <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, voluptates.</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={style.communityContainer}>
+                <div className={style.communityHeader}>
+                    <span>Comunidade</span>
+                </div>
+                <Carousel interval={999999999}>
+                    <Carousel.Item>
+                        <div className={style.communityItemsContainer}>
+                            <div className={style.communityItem}>
+                                <img
+                                    src={require("../assets/img/thumb1.jpg")}
+                                    alt="First slide"
+                                />
+                                <div>
+                                    <h3>First slide label</h3>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </div>
+                            </div>
+                            <div className={style.communityItem}>
+                                <img
+                                    src={require("../assets/img/thumb1.jpg")}
+                                    alt="First slide"
+                                />
+                                <div>
+                                    <h3>First slide label</h3>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </div>
+                            </div>
+                            <div className={style.communityItem}>
+                                <img
+                                    src={require("../assets/img/thumb1.jpg")}
+                                    alt="First slide"
+                                />
+                                <div>
+                                    <h3>First slide label</h3>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className={style.communityItemsContainer}>
+                            <div className={style.communityItem}>
+                                <img
+                                    src={require("../assets/img/thumb1.jpg")}
+                                    alt="First slide"
+                                />
+                                <div>
+                                    <h3>First slide label</h3>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </div>
+
+                            </div>
+                            <div className={style.communityItem}>
+                                <img
+                                    src={require("../assets/img/thumb1.jpg")}
+                                    alt="First slide"
+                                />
+                                <div>
+                                    <h3>First slide label</h3>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </div>
+
+                            </div>
+                            <div className={style.communityItem}>
+                                <img
+                                    src={require("../assets/img/thumb1.jpg")}
+                                    alt="First slide"
+                                />
+                                <div>
+                                    <h3>First slide label</h3>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <div className={style.communityItemsContainer}>
+                            <div className={style.communityItem}>
+                                <img
+                                    src={require("../assets/img/thumb1.jpg")}
+                                    alt="First slide"
+                                />
+                                <div>
+                                    <h3>First slide label</h3>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </div>
+
+                            </div>
+                            <div className={style.communityItem}>
+                                <img
+                                    src={require("../assets/img/thumb1.jpg")}
+                                    alt="First slide"
+                                />
+                                <div>
+                                    <h3>First slide label</h3>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </div>
+
+                            </div>
+                            <div className={style.communityItem}>
+                                <img
+                                    src={require("../assets/img/thumb1.jpg")}
+                                    alt="First slide"
+                                />
+                                <div>
+                                    <h3>First slide label</h3>
+                                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </Carousel.Item>
+                </Carousel>
+
             </div>
         </div>
     );
